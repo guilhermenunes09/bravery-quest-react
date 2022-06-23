4c2adccb5c49186025ec4d2ee3c8692932e294ca
Guilherme Nunes <guilhermenunes09@protonmail.com>
2022-06-23 06:44:22 -0400

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import NavBar from './components/NavBar';
 import Questions from './questions/Questions'
 import QuestionsShow from './questions/QuestionsShow'
+
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <NavBar />
         <Questions />
         <QuestionsShow />
       </header>
