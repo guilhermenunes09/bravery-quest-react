673bd83d737a5fb64af1d787af0a7b374605f564
Guilherme Nunes <guilhermenunes09@protonmail.com>
2022-06-23 01:45:27 -0400

    added mui/material design to the project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Questions from './questions/questions'
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Questions />
+        <Button variant="contained"> Olá Mundo</Button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/questions/questions.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Questions extends Component {
+  render() {
+    return(
+      <>
+        Olá mundo!
+      </>
+    )
+  }
+}
+
+export default Questions; // Don’t forget to use export default!
