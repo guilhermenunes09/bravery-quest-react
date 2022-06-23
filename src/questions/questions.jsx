2ba213306e26c282df0fe403da577544c99a1ab8
Guilherme Nunes <guilhermenunes09@protonmail.com>
2022-06-23 04:41:23 -0400

    added layout to question page

--- src/questions/questions.jsx
+++ src/questions/questions.jsx
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import { styled } from '@mui/material/styles';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 import { createQuestion } from '../services/QuestionService';
 
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 class Questions extends Component {
 
   constructor(props) {
@@ -33,39 +43,54 @@ class Questions extends Component {
     const question = { title: this.state.title, question: this.state.question };
     createQuestion(question);
   }
+  
 
   render() {
     return(
       <>
-        <Box
-          component="form"
-          sx={{
-            '& .MuiTextField-root': { m: 1, width: '25ch' },
-          }}
-          noValidate
-          autoComplete="off"
-          onSubmit={this.handleSubmit}
-        >
-          <div>
-            <TextField id="outlined-basic" name="title" label="Title" variant="outlined" onChange={this.handleChangeTitle} />
-            
-            <TextField
-              id="filled-multiline-static"
-              name="question"
-              label="Pergunta"
-              multiline
-              rows={4}
-              defaultValue="Default Value"
-              onChange={this.handleChangeQuestion} 
-              variant="filled"
-            />
-
-            <Button type="submit" variant="outlined">Save</Button>
-          </div>
-        </Box>
+      <Grid container spacing={3}>
+            <Grid item xs>
+              <Item>xs</Item>
+            </Grid>
+            <Grid item xs={6}>
+              <Item>
+                <Box
+                  component="form"
+                  sx={{
+                    '& .MuiTextField-root': { m: 1, minWidth: '100%', maxWidth: '100%' },
+                  }}
+                  noValidate
+                  autoComplete="off"
+                  onSubmit={this.handleSubmit}
+                >
+                  <div>
+                    <TextField id="outlined-basic" name="title" label="Title" variant="outlined" onChange={this.handleChangeTitle} />
+                  </div>
+                  <div>
+                    <TextField
+                      id="filled-multiline-static"
+                      name="question"
+                      label="Pergunta"
+                      multiline
+                      rows={4}
+                      defaultValue="Default Value"
+                      onChange={this.handleChangeQuestion} 
+                      variant="filled"
+                    />
+                  </div>
+                  <div>
+                    <Button type="submit" variant="outlined">Save</Button>
+                  </div>
+                </Box>
+              </Item>
+            </Grid>
+            <Grid item xs>
+              <Item>xs</Item>
+            </Grid>
+          </Grid>
       </>
     )
   }
 }
 
-export default Questions; // Don’t forget to use export default!
+export default Questions;
