60b56d21f06a3c89054f665a33bf3d09b71c68a0
Guilherme Nunes <guilhermewnunes@gmail.com>
2022-10-02 17:23:41 -0300

    load questions

--- src/questions/Questions.jsx
+++ src/questions/Questions.jsx
@@ -1,9 +1,36 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import { instance } from '../services/QuestionsService';
 
 function Questions() {
+
+  const [questions, setQuestions] = useState(0);
+
+
+  function handleClick() {
+    console.log("clicked")
+    instance.get(`questions`)
+    .then(res => {
+      console.log('res', res)
+      setQuestions(res.data)
+    })
+  }
+
   return (
     <>
       Questions
+      <div className='cursor-pointer hover:text-blue-600' onClick={handleClick}>
+        Click me
+        
+        {questions && questions.map(function(question, id) {
+          return (
+            <ul>
+              <li key={id}>{question.title}</li>
+              <li>{question.question}</li>
+            </ul>
+          )
+        })}
+
+      </div>
     </>
   )
 }
---
+++ src/services/QuestionsService.js
@@ -0,0 +1,10 @@
+
+import axios from 'axios';
+
+const instance = axios.create({
+   baseURL: process.env.REACT_APP_LOCALHOST
+});
+
+export {
+   instance
+};
