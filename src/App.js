0fb99f129025d212577da00958cda1ba06948bad
Guilherme Nunes <guilhermewnunes@gmail.com>
2022-10-05 16:26:03 -0300

    make the scrollbar always visible

--- src/App.js
+++ src/App.js
@@ -22,7 +22,6 @@ function App() {
         <br />
           <div className="main-container">
             <div className="col-span-full lg:col-span-4 flex justify-between cursor-pointer">
-              
               { location.pathname !== "/" &&
                 <div onClick={handleGoBack}>
                   <svg xmlns="http://www.w3.org/2/000/svg" transform="scale(-1 1)" width="24" height="24" viewBox="0 0 24 24"><path fill="gray" d="M22 12l-20 12 7.289-12-7.289-12z"/></svg>
@@ -31,6 +30,7 @@ function App() {
               { location.pathname === "/" &&
                 <div />
               }
+
               <div className="self-right cursor-pointer">
                 { location.pathname !== "/questions/new" &&
                   <Link to="/questions/new" class="button-new-question">
@@ -38,7 +38,6 @@ function App() {
                   </Link>
                 }
               </div>
-            
             </div>
           
             <div className="col-span-full lg:col-span-1">
