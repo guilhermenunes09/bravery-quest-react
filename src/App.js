f3348990b6ffa3e6d38d48b97aa0f599bc2e0594
Guilherme Nunes <guilhermewnunes@gmail.com>
2022-10-06 19:17:53 -0300

    created login page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { NavBar } from "./components/NavBar";
 import { QuestionsShow } from "./questions/QuestionsShow"
 import { QuestionsCreate } from "./questions/QuestionsCreate";
 import { Questions } from "./questions/Questions"
+import { Login } from "./authentication/Login"
 
 
 function App() {
@@ -22,17 +23,14 @@ function App() {
         <br />
           <div className="main-container">
             <div className="col-span-full lg:col-span-8 flex justify-between cursor-pointer mb-20">
-              { location.pathname !== "/" &&
+              { location.pathname !== "/" && location.pathname !== "/login" &&
                 <div className="arrow-icon" onClick={handleGoBack}>
                   <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm0 1c6.071 0 11 4.929 11 11s-4.929 11-11 11-11-4.929-11-11 4.929-11 11-11zm-4.828 11.5l4.608 3.763-.679.737-6.101-5 6.112-5 .666.753-4.604 3.747h11.826v1h-11.828z"/></svg>
                 </div>
               }
-              { location.pathname === "/" &&
-                <div />
-              }
-
+            
               <div className="self-right cursor-pointer">
-                { location.pathname !== "/questions/new" &&
+                { location.pathname !== "/questions/new" && location.pathname !== "/login" &&
                   <Link to="/questions/new" class="button-new-question">
                     New Question
                   </Link>
@@ -49,6 +47,7 @@ function App() {
                 <Route path="/questions/:questionId" exact element={<QuestionsShow/>} />
                 <Route path="/questions/new" exact element={<QuestionsCreate />} />
                 <Route path="/questions" exact element={<Questions/>} />
+                <Route path="/login" exact element={<Login/>} />
                 <Route path="/" exact element={<Questions/>} />
               </Routes>
             </div>
