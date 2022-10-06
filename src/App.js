8280c29a01459f1797f71be17b984f999f93bb0a
Guilherme Nunes <guilhermewnunes@gmail.com>
2022-10-06 04:01:51 -0300

    design improvements

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
         <NavBar />
         <br />
           <div className="main-container">
-            <div className="col-span-full lg:col-span-4 flex justify-between cursor-pointer">
+            <div className="col-span-full lg:col-span-8 flex justify-between cursor-pointer">
               { location.pathname !== "/" &&
                 <div className="arrow-icon" onClick={handleGoBack}>
                   <svg xmlns="http://www.w3.org/2/000/svg" transform="scale(-1 1)" width="24" height="24" viewBox="0 0 24 24"><path d="M22 12l-20 12 7.289-12-7.289-12z"/></svg>
@@ -44,7 +44,7 @@ function App() {
               
             </div>
 
-            <div className="col-span-full lg:col-span-2">
+            <div className="col-span-full lg:col-span-6">
               <Routes>
                 <Route path="/questions/:questionId" exact element={<QuestionsShow/>} />
                 <Route path="/questions/new" exact element={<QuestionsCreate />} />
@@ -52,6 +52,7 @@ function App() {
                 <Route path="/" exact element={<Questions/>} />
               </Routes>
             </div>
+            
             <div className="col-span-full lg:col-span-1">
               
             </div>
