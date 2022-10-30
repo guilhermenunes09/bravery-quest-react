a28b40cbf4f030541f6e7808f43277a8ea5f52e4
Guilherme Nunes <guilhermewnunes@gmail.com>
2022-10-30 15:55:40 -0300

    updated sidebar

--- src/App.js
+++ src/App.js
@@ -21,62 +21,56 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header mb-3">
         <NavBar />
-        <br />
-          <div className={`main-container ${ location.pathname === '/login'  || location.pathname == "/login/true" || location.pathname === '/sign-up' ? "justify-items-center" : ""}`}>
-            
-            <div className="col-span-full lg:col-span-1">
-              
-            </div>
-            
-            <div className="col-span-full lg:col-span-6 flex justify-between cursor-pointer mb-3">
-              { location.pathname !== "/" && location.pathname !== "/login" && location.pathname !== "/login/true" && location.pathname !== "/sign-up" &&
-                <div className="arrow-icon" onClick={handleGoBack}>
-                  <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm0 1c6.071 0 11 4.929 11 11s-4.929 11-11 11-11-4.929-11-11 4.929-11 11-11zm-4.828 11.5l4.608 3.763-.679.737-6.101-5 6.112-5 .666.753-4.604 3.747h11.826v1h-11.828z"/></svg>
-                </div>
-              }
+      </header>
 
-              { location.pathname === "/" &&
-                <div></div>
-              }
-              
-              <div className="self-right cursor-pointer">
-                { location.pathname !== "/questions/new" && location.pathname !== "/login" && location.pathname !== "/login/true" && location.pathname !== "/sign-up" &&
-                  <Link to="/questions/new" class="button-new-question">
-                    Ask any Question
-                  </Link>
-                }
-              </div>
+      <div>
+        <div className="flex justify-between cursor-pointer mb-3">
+          { location.pathname !== "/" && location.pathname !== "/login" && location.pathname !== "/login/true" && location.pathname !== "/sign-up" &&
+            <div className="arrow-icon" onClick={handleGoBack}>
+              <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm0 1c6.071 0 11 4.929 11 11s-4.929 11-11 11-11-4.929-11-11 4.929-11 11-11zm-4.828 11.5l4.608 3.763-.679.737-6.101-5 6.112-5 .666.753-4.604 3.747h11.826v1h-11.828z"/></svg>
             </div>
+          }
 
-            <div className="col-span-full lg:col-span-1">
-              
-            </div>
+          { location.pathname === "/" &&
+            <div></div>
+          }
           
-            <div className="col-span-full lg:col-span-1">
-              
-            </div>
-
-            <div className="col-span-full lg:col-span-6">
-              <Routes>
-                <Route path="/questions/:questionId" exact element={<QuestionsShow/>} />
-                <Route path="/questions/new" exact element={<QuestionsCreate />} />
-                <Route path="/questions" exact element={<Questions/>} />
-                <Route path="/login" element={<Login/>} />
-                <Route path="/login/:goback" element={<Login/>} />
-                <Route path="/sign-up" exact element={<SignUp/>} />
-                <Route path="/my-account" exact element={<MyAccount/>} />
-                <Route path="/" exact element={<Questions/>} />
-                <Route path="*" element={<NotFoundPage />} />
-              </Routes>
-            </div>
-
-            <div className="col-span-full lg:col-span-1">
-              
+          <div className="self-right cursor-pointer">
+            { location.pathname !== "/questions/new" && location.pathname !== "/login" && location.pathname !== "/login/true" && location.pathname !== "/sign-up" &&
+              <Link to="/questions/new" class="button-new-question">
+                Ask any Question
+              </Link>
+            }
+          </div>
+        </div>
+        <div className="flex">
+          <div className="w-10 flex-auto p-3">
+            <div className="card-menu-category hidden">
+              REACT
             </div>
           </div>
-      </header>
+
+          <div className="grow p-3">
+            <Routes>
+              <Route path="/questions/:questionId" exact element={<QuestionsShow/>} />
+              <Route path="/questions/new" exact element={<QuestionsCreate />} />
+              <Route path="/questions" exact element={<Questions/>} />
+              <Route path="/login" element={<Login/>} />
+              <Route path="/login/:goback" element={<Login/>} />
+              <Route path="/sign-up" exact element={<SignUp/>} />
+              <Route path="/my-account" exact element={<MyAccount/>} />
+              <Route path="/" exact element={<Questions/>} />
+              <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+          </div>
+
+          <div className="w-10 flex-auto p-3">
+            
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './App.css';
 import './index.css';
+import './questions/questions.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
